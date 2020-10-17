2784f2e660b643f49b71dc83b1fd7186a0fbf663
Nurdine Rajabo <nurdinerajabo@gmail.com>
2020-10-18 01:37:13 +0200

    Addes heroku postg

--- muvabe/settings/prod.py
+++ muvabe/settings/prod.py
@@ -2,6 +2,19 @@ from muvabe.settings.base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbcfvfu89vshg7',
+        'USER': 'cqpbambwqaevbk',
+        'PASSWORD': '0ad686983a933db4df5be8666cc28ffbe1957eff09830f781c275392ac579c26',
+        'HOST': 'ec2-34-200-72-77.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+
 
 try:
     from muvabe.settings.local import *
